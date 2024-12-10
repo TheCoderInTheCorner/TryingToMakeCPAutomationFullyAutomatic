7e5cb56a8500c8118009dfc1cd935efcd1bc0f7f
TheCoderInTheCorner <109489785+TheCoderInTheCorner@users.noreply.github.com>
2024-12-09 20:42:45 -0500

    Update automation.py
    MADE IT PRETTY

--- automation.py
+++ automation.py
@@ -1,76 +1,118 @@
 import subprocess
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-url = "https://www.uscyberpatriot.org/Pages/Readme/cp17_tr2_m_ubu22_readme_jsgrxe68wc.aspx"
-response = requests.get(url)
+'''
+
+
+Created By Basu Khadka On 12/9/2024 7:38PM
+
+This Linux Hardening Script Will Accomplish The Following
+
+[1]. Delete and add users based off of readme link
+[2]. UFW settings
+[3]. Find and delete forbidden media files
+[4]. Try to find a list of common "hacking" tools
+[5]. Fix insecure permissions on shadow file
+[6].
+[7].
+[8].
+[9].
+[10].
+
+
+'''
+
+
+# =============== Functions ===============
+
+def get_readme_text(url: str):
+    response = requests.get(url)
+
+    # Retrieving the html text from the cyberpatriots website
+    if response.status_code == 200:
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        return soup.get_text()
+    else:
+        print(f"Failed to retrieve webpage. Status code: {response.status_code}")
+
+
+def strip_all_items_in_list(targetList: list):
+    for i in range(len(targetList)):
+        targetList[i] = targetList[i].strip()
+
+
+def return_code(command, passed, failed):
+    if command.returncode == 0:
+        print("-"*20)
+        print(passed)
+        print("-"*20)
+    else:
+        print("!"*20)
+        print(failed)
+        print("!"*20)
+# =========================================
+
+
+# =============== User Adding And Deleting ===============
 
 whoami = 'perry'
-#whoami = subprocess.run('whoami', shell=True, capture_output=True, text=True).stdout
+# whoami = subprocess.run('whoami', shell=True, capture_output=True, text=True).stdout
 whoami.strip()
 
+# getting text, getting authorized users, and cleaning data
+readMeText = get_readme_text('https://www.uscyberpatriot.org/Pages/Readme/cp17_tr2_m_ubu22_readme_jsgrxe68wc.aspx')  # !!!!!!!!!! CHANGE THE URL !!!!!!!!!!
+authorizedUsers = readMeText[readMeText.find(whoami):readMeText.find('Competition Guidelines')]
+authorizedUsers = authorizedUsers.splitlines()
 
-# Retrieving the html text from the cyberpatriots website
-if response.status_code == 200:
-    html_content = response.text
-    soup = BeautifulSoup(html_content, "html.parser")
-    htmlText = soup.get_text()
-else:
-    print(f"Failed to retrieve webpage. Status code: {response.status_code}")
+# cleaning data
+authorizedUsers = [item for item in authorizedUsers if 'password' not in item and item != '' and item != 'Authorized Users:']
+authorizedUsers.pop(0)
+strip_all_items_in_list(authorizedUsers)
 
+# getting list of all users on computer
+allUsers = subprocess.run('ls /home', shell=True, capture_output=True, text=True).stdout.splitlines()
+strip_all_items_in_list(allUsers)
 
-# Cleaning the Data
-if whoami in htmlText:
-    AuthorizedUsers = htmlText[htmlText.find(whoami):htmlText.find('Competition Guidelines')]
-    AuthorizedUsers = AuthorizedUsers.splitlines()
+# converting lists to set
+authorizedUsers = set(authorizedUsers)
+allUsers = set(allUsers)
 
-for i in AuthorizedUsers:
-    AuthorizedUsers[AuthorizedUsers.index(i)] = i.strip()
+# users to remove and those to add
+usersToRemove = allUsers.difference(authorizedUsers)
+usersToAdd = authorizedUsers.difference(allUsers)
 
-AuthorizedUsers = [item for item in AuthorizedUsers if 'password' not in item and item != '' and item != 'Authorized Users:']
-AuthorizedUsers.pop(0)
+# Actually Deleting And Adding Users
 
-# Deleting and Adding Users
-AuthorizedUsers = set(AuthorizedUsers)
-AllUsers = subprocess.run('ls /home', shell=True, capture_output=True, text=True).stdout.splitlines()
-for i in AllUsers:
-    AllUsers[AllUsers.index(i)] = i.strip()
-AllUsers = set(AllUsers)
-
-usersToRemove=AllUsers.difference(AuthorizedUsers)
-usersToAdd=AuthorizedUsers.difference(AllUsers)
-
-'''
-HEY!! USE ANSI ESCAPE CODES TO MAKE THIS LOOK GOOD LATER
-'''
-print(f'Found {len(usersToAdd)} Users That Need To Be Added') #Using ANSI escape codes
+# Adding Users
+print(f'\033[1;32;5mFound {len(usersToAdd)} Users To Add\033[0m')
 for i in usersToAdd:
-    ans=input(f'Would You Like To Add User {i}?').lower()
+    ans = input(f'Would You Like To Add User {i} (y/n)?').lower()
+    while ans not in ['y', 'n']:
+        ans = input(f'WOULD YOU LIKE TO DELETE {i}?').lower()
     if ans == 'y':
         cmd = 'sudo adduser ' + i
-        adding_User=subprocess.run(cmd, shell=True)
+        addingUser = subprocess.run(cmd, shell=True)
+        return_code(addingUser, f'\033[1;32mSuccessfully Added User {i}\033[0m', f'\033[1;91mERROR: COULD NOT ADD USER {i}\033[0m')
     else:
-        print(f'DID NOT ADD USER {i}')
+        print(f'\033[1;91mDID NOT ADD USER {i}\033[0m')
 
-print(f'FOUND {len(usersToAdd)} USERS THAT NEED TO BE DELETED')
+# SAFE GUARD TO MAKE SURE YOU DO NOT DELETE YOUR SELF
+usersToRemove.discard(whoami)
+#####################################################
 
-whoami = subprocess.run('whoami', shell=True, capture_output=True, text=True).stdout
-whoami.strip()
-if whoami.strip in usersToRemove 
-usersToRemove.pop()
+# deleting users
+print(f'\033[1;91mFOUND {len(usersToAdd)} USERS THAT NEED TO BE DELETED\033[0m')
 for i in usersToRemove:
-    ans=input(f'WOULD YOU LIKE TO DELETE {i}?').lower()
+    ans = input(f'WOULD YOU LIKE TO DELETE {i}?').lower()
+    while ans not in ['y', 'n']:
+        ans = input(f'WOULD YOU LIKE TO DELETE {i}?').lower()
     if ans == 'y':
-        cmd = 'sudo deluser ' + i + ' --remove-home'
-        deleting_User=subprocess.run(cmd, shell=True)
+        cmd = f'sudo deluser {i} --remove-home'
+        deletingUser = subprocess.run(cmd, shell=True)
+        return_code(deletingUser, f'\033[32mSuccessfully \033[1;91mDELETED\033[0;32m User {i}\033[0m', f'\033[1;91mERROR: COULD NOT ADD USER {i}\033[0m')
     else:
-        print(f'DID NOT DELETE USER {i}')
+        print(f'\033[1;32mDID NOT DELETE USER {i}')
 
-'''
-To DO LIST
-[1] Make it so user running automation cannto remove themselve
-[2] Make it cool letters and cool colors
-[3] Make everything look nice AND AND structure everything so it looks pretty
-
-'''
 
